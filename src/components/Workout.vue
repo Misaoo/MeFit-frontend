29028d4ffad61b75f065e511f963d4896feae708
Jesper <jesper_truedsson@hotmail.com>
2019-11-08 15:39:32 +0100

    added lots of components

--- src/components/Workout.vue
+++ src/components/Workout.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
-    <div class="card">
-        <img src="https://source.unsplash.com/random/300x300" class="card-img-top" alt="#">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
+    <h1>Workout showing all the selected exercises</h1>
   </div>
 </template>
 
@@ -18,19 +11,5 @@ export default {
 </script>
 
 <style>
-/* Desktop */
-  .card {
-    width: 240px;
-    margin: 10px;
-    background: rgba(70, 155, 172, 0.5);
-  }
-/* Mobile */
-@media (min-width: 360px) and (max-width: 600px) {
 
-} 
-    
-/* Tablet */
-@media (min-width: 768px) and (max-width: 1024px) {
-    
-} 
 </style>
